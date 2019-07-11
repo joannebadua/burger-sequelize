6905f405d492b9b9650f3d15853dbfec368ba38b
Joanne Badua <joannebadua@gmail.com>
2019-07-11 16:39:45 -0700

    create burgers database, create burgers tablein schema.sql

--- schema.sql
+++ schema.sql
@@ -1,4 +1,13 @@
--- Drops the burgers if it exists currently --
-DROP DATABASE IF EXISTS burgers;
+-- Drops the burgers_db if it exists currently --
+DROP DATABASE IF EXISTS burgers_db;
+
 -- Creates the "burgers" database --
-CREATE DATABASE burgers;
+CREATE DATABASE burgers_db;
+USE burgers_db;
+CREATE TABLE burgers
+(
+    id int NOT NULL AUTO_INCREMENT,
+    burger_name varchar(255) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
+);
